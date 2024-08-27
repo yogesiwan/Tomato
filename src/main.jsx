570da9dbcc4b2a461581842003eef12303f039b5
yogesh <yogesh@yogeshs-MacBook-Air.local>
2024-08-27 07:25:41 +0530

    hash

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@
 import ReactDom from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {BrowserRouter} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom'
 import StoreContextProvider from './context/StoreContext.jsx'
 
 ReactDom.createRoot(document.getElementById('root')).render(
-   <BrowserRouter basename="/Tomato">
+   <HashRouter>
     <StoreContextProvider>
     <App />
     </StoreContextProvider>
-   </BrowserRouter>
+   </HashRouter>
 
 )
